e6000051bf084bea176cafa8f6bf2fda08f13bb6
Victor Hugo Marques <80768509+Victordelete@users.noreply.github.com>
2023-01-17 20:33:36 -0300

    Update globals

--- SCRIPTS/globals.py
+++ SCRIPTS/globals.py
@@ -9,7 +9,11 @@ from datetime import datetime as datetime
 ###############################################
 #definições gerais
 root_path = os.path.dirname((os.path.realpath(__file__)))
+<<<<<<< Updated upstream
 datalake_file = os
+=======
+datalake_file = "DADOS/Federal_Deposit_Insurance_Corporation_FDIC_Insured_Banks.csv"
+>>>>>>> Stashed changes
 
 ###############################################
 #definições - sistema de ingestão 
