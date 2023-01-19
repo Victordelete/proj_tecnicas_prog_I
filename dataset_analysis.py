4f3fe06db919bc3405fa174cce61bb8c30140a81
Victor <victorh_delete@hotmail.com>
2023-01-19 07:34:39 -0300

    Select by estado

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -14,9 +14,19 @@ def criar_tabela_score_bancos_porto_rico() -> pd.DataFrame:
 
     criar_tabela_score_bancos_porto_rico.to_csv(ROOT_PATH + r"\dataset_kaggle.com\g_score_bancos_porto_rico.csv")
     
+def visao_por_estado() -> pd.DataFrame:
+    colunas_mantidas = ["CNTRYNAB", "STNAMEBR"]
+    data = pd.read_csv(ROOT_PATH + r"\dataset_kaggle.com\s_seguro_depositos_federais.csv")
+    for coluna_tabela in data.columns.values:
+        if coluna_tabela not in colunas_mantidas:
+            data = data.drop(columns=coluna_tabela)
+    data.to_csv(ROOT_PATH + r"\dataset_kaggle.com\visao_por_estado.csv")
+    
+
 
 ################################################
 
 if __name__ == '__main__':
     criar_tabela_score_bancos_porto_rico()
+    visao_por_estado()
     print(f'{dt.now()} [INFO] Finalizado a analise com sucesso.')
