882a1c00f3fc4d539f3e84d27d887e3435ed553e
Victor <victorh_delete@hotmail.com>
2023-01-20 06:37:15 -0300

    Integracao analises do Cleber
    Commit para integração das visualizações de dados que o Cleber criou, com a pipeline proposta.

--- data_analysis.py
+++ data_analysis.py
@@ -44,11 +44,69 @@ def participacao_mercado() ->pd.DataFrame:
     top_15 = participacao_individual.head(15)
     top_15.to_csv(ROOT_PATH + r"\dataset_kaggle.com\participacao_mercado.csv")
 
+def ativos_bancos_estado() -> pd.DataFrame:
+    # Verificado que no dataset existem alguns países e ilhas agregadas aos EUA como, por exemplo, 
+    # A'merican Samoa e Marshall Islands
+    # Para uma análise mais precisa utilizaremos somente os 50 estados oficiais pertencentes aos EUA
 
-################################################
+    # Primeiramente utilizando a biblioteca 'us' disponível em https://pypi.org/project/us/ para criar uma lista de todos os estados
+
+    ''' Código para importas dados da biblioteca US
+    from us import states
+    estados_oficiais = [state.name for state in states.STATES]    
+    '''
+   
+    # Limpando o DataFrame, utilizando os dados de 'STNAMEBR' que estão em 'estados_oficiais'
+    data = pd.read_csv(DATALAKE_FILE, low_memory=False)
+    data = data[data['STNAMEBR'].isin(estados_oficiais)]
+
+    # Agrupando os bancos pelo estado, utilizando a coluna STALPBR
+    bancos_por_estado = data.groupby('STALPBR')
+
+    # Calcular a média dos ativos de cada estado
+    media_por_estado = bancos_por_estado['ASSET'].sum()
+
+    # Juntando 'informcaoes_por_estado' com 'media_por_estado'
+
+    informcaoes_por_estado = data[['STALPBR', 'STNAMEBR']].drop_duplicates()
+    media_por_estado = pd.merge(media_por_estado, informcaoes_por_estado, on='STALPBR')
+
+    # Ordenando por ativos 
+    media_por_estado = media_por_estado.sort_values(by='ASSET', ascending=False)
+
+    # Criando um gráfico com os dados
+    # Para melhor visualização em formato gráfico, utilizamos somente os 15 maiores
+    # Exibindo somente os 15 maiores
+
+    top_15_estados = media_por_estado.head(15)
+    top_15_estados.to_csv(ROOT_PATH + r"\dataset_kaggle.com\ativos_bancos_estado.csv")
+
+    # Os dados podem ser adquiridos diretamente pela API do bea.gov, mas como são dados simples e 
+    # de fácil importação por csv foi optado por esta maneira
+    # Carregar os dados e apresentar cabeçalho do df (DataFrame)
+    df_gdp = pd.read_csv(ROOT_PATH + r"\dataset_kaggle.com\Gross_Domestic_Product_by_State_2021.csv")
+
+    # Realizando a junção dos dados do dataset estadual com o valor do PIB por estado
+    juncao_ativos_com_pib = media_por_estado.merge(df_gdp, left_on='STNAMEBR', right_on='state')
+
+    # Excluindo a coluna 'state' do DataFrame gerado pela união
+    juncao_ativos_com_pib = juncao_ativos_com_pib.drop('state', axis=1)
+
+    # Somando valores das colunas Asset e gdp, para calcular o total. Criando as colunas 'ASSET_%' e '2021gdp_%'
+    # representam o valor em porcentagem do total para cada estado
+
+    juncao_ativos_com_pib['ASSET_%'] = juncao_ativos_com_pib['ASSET'].div(juncao_ativos_com_pib['ASSET'].sum()) * 100
+    juncao_ativos_com_pib['2021gdp_%'] = juncao_ativos_com_pib['2021gdp'].div(juncao_ativos_com_pib['2021gdp'].sum()) * 100
+
+    # Organizando o DataFrame pelo valor do ativo de cada estado
+
+    juncao_ativos_com_pib = juncao_ativos_com_pib.sort_values(by='ASSET', ascending=False)
+    juncao_ativos_com_pib.to_csv(ROOT_PATH + r"\dataset_kaggle.com\ativos_bancos_pib_estado.csv")
+    ################################################
 
 if __name__ == '__main__':
     criar_tabela_score_bancos_porto_rico()
     visao_por_estado()
     participacao_mercado()
+    ativos_bancos_estado()
     print(f'{dt.now()} [INFO] Finalizado a analise com sucesso.')
--- globals.py
+++ globals.py
@@ -32,3 +32,13 @@ DATA_APRESENTATION = "data_analysis.ipynb"
 ###############################################
 #dados de origem
 DATALAKE_FILE = "dataset_kaggle.com\Federal_Deposit_Insurance_Corporation_FDIC_Insured_Banks.csv"
+
+###############################################
+#Define dados
+estados_oficiais = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 
+                        'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 
+                        'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
+                        'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 
+                        'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
+                        'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 
+                        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
